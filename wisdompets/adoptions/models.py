79d2298642fed2fb6fdcfc00744c7dd299a2a541
dharitha23 <harithadesikan@outlook.com>
2021-04-17 07:10:43 -0400

    Created Models and migrations

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    SEX_CHOICES = [('M','Male'), ('F','Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=20)
+    breed = models.CharField(max_length=20, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccines = models.ManyToManyField('Vaccine', blank=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
