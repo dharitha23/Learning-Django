276dad62c59ff845d754e1decd686f82f454989d
dharitha23 <harithadesikan@outlook.com>
2021-04-17 09:36:16 -0400

    Refactoring

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -4,4 +4,4 @@ from .models import Pet,Vaccine
 
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
-    list_display = ['name','species','breed','sex','age','vaccines']
+    list_display = ['name','species','breed','sex','age']
